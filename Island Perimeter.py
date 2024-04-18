837637b2f72a82a9e04acd6726d789473022345a
SRISABARI <87270623+SRISABARI46@users.noreply.github.com>
2024-04-18 10:33:29 -0700

    Create Island Perimeter.py
    You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
    
    Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
    
    The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
    
     
    
    Example 1:
    
    
    Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
    Output: 16
    Explanation: The perimeter is the 16 yellow stripes in the image above.
    Example 2:
    
    Input: grid = [[1]]
    Output: 4
    Example 3:
    
    Input: grid = [[1,0]]
    Output: 4

---
+++ Island Perimeter.py
@@ -0,0 +1,16 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
+        p=0    #perimenter
+        c=0    #connection
+        for i in range(0,row):
+            for j in range(0,col):
+                if grid[i][j]==1:
+                    p+=4
+                    if i!=0 and grid[i-1][j]==1:
+                        c+=1
+                    if j!=0 and grid[i][j-1]==1:
+                        c+=1
+        return (p-c*2)
+        
